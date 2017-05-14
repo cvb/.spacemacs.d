3bb778a7ba9d26b0aa886b691d24e8bb8af81013
cvb <peter@standalone.su>
2017-05-14 20:36:35 +0300

    Some fancy customizes

--- init.el
+++ init.el
@@ -188,11 +188,15 @@ layers configuration."
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ac-ispell-requires 4 t)
- '(ahs-case-fold-search nil)
- '(ahs-default-range (quote ahs-range-whole-buffer))
- '(ahs-idle-interval 0.25)
+ '(ahs-case-fold-search nil t)
+ '(ahs-default-range (quote ahs-range-whole-buffer) t)
+ '(ahs-idle-interval 0.25 t)
  '(ahs-idle-timer 0 t)
- '(ahs-inhibit-face-list nil)
+ '(ahs-inhibit-face-list nil t)
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   (vector "#4d4d4c" "#c82829" "#718c00" "#eab700" "#4271ae" "#8959a8" "#3e999f" "#d6d6d6"))
  '(ansi-term-color-vector
    [unspecified "#081724" "#ff694d" "#68f6cb" "#fffe4e" "#bad6e2" "#afc0fd" "#d2f1ff" "#d3f9ee"] t)
  '(ccm-ignored-commands
@@ -203,9 +207,23 @@ layers configuration."
    "(do (require 'weasel.repl.websocket) (cemerick.piggieback/cljs-repl (weasel.repl.websocket/repl-env :ip \"127.0.0.1\" :port 9001)))")
  '(clojure-defun-style-default-indent t)
  '(clojure-indent-style :always-indent)
+ '(company-dabbrev-downcase nil)
+ '(company-dabbrev-ignore-case nil)
+ '(company-idle-delay 0.2)
+ '(company-minimum-prefix-length 2)
+ '(company-require-match nil)
+ '(company-statistics-mode nil)
+ '(company-transformers
+   (quote
+    (spacemacs//company-transformer-cancel company-sort-by-occurrence)))
+ '(custom-enabled-themes (quote (clues)))
+ '(custom-safe-themes
+   (quote
+    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default)))
  '(enh-ruby-bounce-deep-indent t)
  '(enh-ruby-deep-arglist t)
- '(fci-rule-color "#475662")
+ '(evil-want-Y-yank-to-eol nil)
+ '(fci-rule-color "#475662" t)
  '(inf-ruby-implementations
    (quote
     (("ruby" . "irb --prompt default --noreadline -r irb/completion")
@@ -228,12 +246,14 @@ layers configuration."
  '(org-pomodoro-short-break-format "~%s")
  '(package-selected-packages
    (quote
-    (direnv nix-sandbox log4e gntp simple-httpd parent-mode gitignore-mode flx evil goto-chg json-snatcher json-reformat diminish nixos-options ghc peg packed pythonic pkg-info epl avy popup package-build autothemer alert seq bind-key bind-map request skewer-mode company psci purescript-mode psc-ide ahk-mode helm-company helm-c-yasnippet company-web web-completion-data company-tern dash-functional tern company-statistics company-nixos-options company-cabal company-anaconda clojure-snippets auto-yasnippet ac-ispell auto-complete ein websocket pcache ob-elixir org minitest hide-comnt anzu highlight flycheck async inf-ruby dash dockerfile-mode docker tablist docker-tramp zenburn-theme yaml-mode ws-butler which-key web-mode toc-org spacemacs-theme spaceline sass-mode ruby-test-mode robe projectile-rails pip-requirements persp-mode paradox organic-green-theme org-plus-contrib open-junk-file omtose-phellack-theme neotree move-text monokai-theme moe-theme material-theme macrostep less-css-mode js2-refactor indent-guide hl-todo hindent help-fns+ helm-themes helm-pydoc helm-projectile helm-descbinds helm-ag haskell-snippets gruvbox-theme grandshell-theme google-translate evil-surround evil-search-highlight-persist evil-mc evil-matchit evil-iedit-state iedit eshell-prompt-extras erlang ensime sbt-mode scala-mode emmet-mode darktooth-theme cyberpunk-theme color-theme-sanityinc-tomorrow clj-refactor hydra yasnippet cider-eval-sexp-fu cider clojure-mode badwolf-theme anaconda-mode ample-theme alchemist ace-window ace-link ace-jump-helm-line smartparens undo-tree elixir-mode helm helm-core haskell-mode markdown-mode projectile magit magit-popup git-commit with-editor f js2-mode s visual-regexp tao-theme zonokai-theme zen-and-art-theme yapfify xterm-color window-numbering web-beautify volatile-highlights visual-regexp-steroids vi-tilde-fringe uuidgen use-package underwater-theme ujelly-theme twilight-theme twilight-bright-theme twilight-anti-bright-theme tronesque-theme toxi-theme tangotango-theme tango-plus-theme tango-2-theme tagedit sunny-day-theme sublime-themes subatomic256-theme subatomic-theme stekene-theme spinner spacegray-theme soothe-theme soft-stone-theme soft-morning-theme soft-charcoal-theme smyx-theme smeargle slim-mode shell-pop seti-theme scss-mode rvm ruby-tools rubocop rspec-mode reverse-theme restart-emacs rbenv rake railscasts-theme queue quelpa pyvenv pytest pyenv-mode py-isort purple-haze-theme pug-mode professional-theme powerline popwin planet-theme phoenix-dark-pink-theme phoenix-dark-mono-theme pcre2el pastels-on-dark-theme paredit orgit org-projectile org-present org-pomodoro org-download oldlace-theme occidental-theme obsidian-theme noflet noctilux-theme nix-mode niflheim-theme naquadah-theme mustang-theme multiple-cursors multi-term monochrome-theme molokai-theme mmm-mode minimal-theme markdown-toc majapahit-theme magit-gitflow lush-theme lorem-ipsum livid-mode live-py-mode linum-relative link-hint light-soap-theme keyfreq json-mode js-doc jinja2-mode jbeans-theme jazz-theme ir-black-theme intero inkpot-theme info+ inflections ido-vertical-mode hy-mode hungry-delete htmlize hlint-refactor highlight-parentheses highlight-numbers highlight-indentation heroku-theme hemisu-theme helm-swoop helm-nixos-options helm-mode-manager helm-make helm-hoogle helm-gitignore helm-flx helm-css-scss hc-zenburn-theme haml-mode gruber-darker-theme gotham-theme golden-ratio gnuplot gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link gh-md gandalf-theme flycheck-mix flx-ido flatui-theme flatland-theme firebelly-theme fill-column-indicator feature-mode farmhouse-theme fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-numbers evil-nerd-commenter evil-magit evil-lisp-state evil-indent-plus evil-exchange evil-escape evil-ediff evil-args evil-anzu eval-sexp-fu espresso-theme eshell-z esh-help elisp-slime-nav edn dumb-jump dracula-theme django-theme define-word darkokai-theme darkmine-theme darkburn-theme dakrone-theme cython-mode company-ghci company-ghc column-enforce-mode colorsarenice-theme color-theme-sanityinc-solarized coffee-mode cmm-mode clues-theme clean-aindent-mode chruby cherry-blossom-theme busybee-theme bundler bubbleberry-theme birds-of-paradise-plus-theme auto-highlight-symbol auto-compile apropospriate-theme anti-zenburn-theme ansible-doc ansible ample-zen-theme alect-themes aggressive-indent afternoon-theme adaptive-wrap)))
+    (winum solarized-theme madhat2r-theme fuzzy flycheck-credo company-quickhelp pos-tip dante direnv nix-sandbox log4e gntp simple-httpd parent-mode gitignore-mode flx evil goto-chg json-snatcher json-reformat diminish nixos-options ghc peg packed pythonic pkg-info epl avy popup package-build autothemer alert seq bind-key bind-map request skewer-mode company psci purescript-mode psc-ide ahk-mode helm-company helm-c-yasnippet company-web web-completion-data company-tern dash-functional tern company-statistics company-nixos-options company-cabal company-anaconda clojure-snippets auto-yasnippet ac-ispell auto-complete ein websocket pcache ob-elixir org minitest hide-comnt anzu highlight flycheck async inf-ruby dash dockerfile-mode docker tablist docker-tramp zenburn-theme yaml-mode ws-butler which-key web-mode toc-org spacemacs-theme spaceline sass-mode ruby-test-mode robe projectile-rails pip-requirements persp-mode paradox organic-green-theme org-plus-contrib open-junk-file omtose-phellack-theme neotree move-text monokai-theme moe-theme material-theme macrostep less-css-mode js2-refactor indent-guide hl-todo hindent help-fns+ helm-themes helm-pydoc helm-projectile helm-descbinds helm-ag haskell-snippets gruvbox-theme grandshell-theme google-translate evil-surround evil-search-highlight-persist evil-mc evil-matchit evil-iedit-state iedit eshell-prompt-extras erlang ensime sbt-mode scala-mode emmet-mode darktooth-theme cyberpunk-theme color-theme-sanityinc-tomorrow clj-refactor hydra yasnippet cider-eval-sexp-fu cider clojure-mode badwolf-theme anaconda-mode ample-theme alchemist ace-window ace-link ace-jump-helm-line smartparens undo-tree elixir-mode helm helm-core haskell-mode markdown-mode projectile magit magit-popup git-commit with-editor f js2-mode s visual-regexp tao-theme zonokai-theme zen-and-art-theme yapfify xterm-color window-numbering web-beautify volatile-highlights visual-regexp-steroids vi-tilde-fringe uuidgen use-package underwater-theme ujelly-theme twilight-theme twilight-bright-theme twilight-anti-bright-theme tronesque-theme toxi-theme tangotango-theme tango-plus-theme tango-2-theme tagedit sunny-day-theme sublime-themes subatomic256-theme subatomic-theme stekene-theme spinner spacegray-theme soothe-theme soft-stone-theme soft-morning-theme soft-charcoal-theme smyx-theme smeargle slim-mode shell-pop seti-theme scss-mode rvm ruby-tools rubocop rspec-mode reverse-theme restart-emacs rbenv rake railscasts-theme queue quelpa pyvenv pytest pyenv-mode py-isort purple-haze-theme pug-mode professional-theme powerline popwin planet-theme phoenix-dark-pink-theme phoenix-dark-mono-theme pcre2el pastels-on-dark-theme paredit orgit org-projectile org-present org-pomodoro org-download oldlace-theme occidental-theme obsidian-theme noflet noctilux-theme nix-mode niflheim-theme naquadah-theme mustang-theme multiple-cursors multi-term monochrome-theme molokai-theme mmm-mode minimal-theme markdown-toc majapahit-theme magit-gitflow lush-theme lorem-ipsum livid-mode live-py-mode linum-relative link-hint light-soap-theme keyfreq json-mode js-doc jinja2-mode jbeans-theme jazz-theme ir-black-theme intero inkpot-theme info+ inflections ido-vertical-mode hy-mode hungry-delete htmlize hlint-refactor highlight-parentheses highlight-numbers highlight-indentation heroku-theme hemisu-theme helm-swoop helm-nixos-options helm-mode-manager helm-make helm-hoogle helm-gitignore helm-flx helm-css-scss hc-zenburn-theme haml-mode gruber-darker-theme gotham-theme golden-ratio gnuplot gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link gh-md gandalf-theme flycheck-mix flx-ido flatui-theme flatland-theme firebelly-theme fill-column-indicator feature-mode farmhouse-theme fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-numbers evil-nerd-commenter evil-magit evil-lisp-state evil-indent-plus evil-exchange evil-escape evil-ediff evil-args evil-anzu eval-sexp-fu espresso-theme eshell-z esh-help elisp-slime-nav edn dumb-jump dracula-theme django-theme define-word darkokai-theme darkmine-theme darkburn-theme dakrone-theme cython-mode company-ghci company-ghc column-enforce-mode colorsarenice-theme color-theme-sanityinc-solarized coffee-mode cmm-mode clues-theme clean-aindent-mode chruby cherry-blossom-theme busybee-theme bundler bubbleberry-theme birds-of-paradise-plus-theme auto-highlight-symbol auto-compile apropospriate-theme anti-zenburn-theme ansible-doc ansible ample-zen-theme alect-themes aggressive-indent afternoon-theme adaptive-wrap)))
  '(paradox-github-token t)
  '(powerline-default-separator nil)
  '(powerline-height 20)
  '(powerline-text-scale-factor 0.7)
- '(python-shell-interpreter "python3" t)
+ '(psc-ide-add-import-on-completion t t)
+ '(psc-ide-rebuild-on-save nil t)
+ '(python-shell-interpreter "python3")
  '(ring-bell-function (quote ignore))
  '(rspec-use-rake-when-possible t)
  '(safe-local-variable-values
@@ -258,6 +278,28 @@ layers configuration."
      (elixir-enable-compilation-checking))))
  '(sp-show-pair-from-inside t)
  '(standard-indent 2)
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#c82829")
+     (40 . "#f5871f")
+     (60 . "#eab700")
+     (80 . "#718c00")
+     (100 . "#3e999f")
+     (120 . "#4271ae")
+     (140 . "#8959a8")
+     (160 . "#c82829")
+     (180 . "#f5871f")
+     (200 . "#eab700")
+     (220 . "#718c00")
+     (240 . "#3e999f")
+     (260 . "#4271ae")
+     (280 . "#8959a8")
+     (300 . "#c82829")
+     (320 . "#f5871f")
+     (340 . "#eab700")
+     (360 . "#718c00"))))
+ '(vc-annotate-very-old-color nil)
  '(wakatime-api-key "f3f4f28d-ae2d-48c9-a09e-3cb2d00f47e3")
  '(wakatime-cli-path "/Users/cvb/.pyenv/shims/wakatime")
  '(web-mode-code-indent-offset 2)
@@ -271,9 +313,21 @@ layers configuration."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((((class color) (min-colors 16777216)) (:foreground "#C0E9F6" :background "#110B14")) (((class color) (min-colors 88)) (:foreground "#fff" :background "#000")) (((class color) (min-colors 16)) (:foreground "#fff" :background "#000")) (((class color) (min-colors 8)) (:foreground "#fff" :background "#000"))))
+ '(company-tooltip ((t (:background "DarkGoldenrod1"))))
+ '(company-tooltip-selection ((t (:background "steel blue"))))
  '(ein:cell-input-area ((t (:background "#202020"))))
+ '(helm-ff-directory ((t (:foreground "#55B1E2"))))
+ '(helm-ff-file ((t nil)))
+ '(helm-selection ((t (:background "dark green"))))
+ '(lazy-highlight ((t (:background "#77bbdd"))))
  '(org-done ((t (:foreground "forest green" :weight bold))))
  '(org-meta-line ((t (:foreground "#9D9D9D" :height 1.0))))
  '(org-pomodoro-mode-line ((t nil)))
  '(org-pomodoro-mode-line-break ((t (:foreground "pale green"))))
- '(org-todo ((t (:foreground "#D9D9D9" :weight bold)))))
+ '(org-todo ((t (:foreground "#D9D9D9" :weight bold))))
+ '(show-paren-match ((t (:background nil :foreground "spring green"))))
+ '(show-paren-mismatch ((t (:background "red1"))))
+ '(whitespace-space-after-tab ((t (:background "default" :foreground "#D9D9D9"))))
+ '(whitespace-space-before-tab ((t (:foreground "firebrick"))))
+ '(whitespace-trailing ((t (:background "#555555")))))
